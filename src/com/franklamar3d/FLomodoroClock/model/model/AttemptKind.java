1ea00cfeac22a32328a1767962b447530936bb9d
Lamar, Frank <frankie_lamar1@homedepot.com>
2016-10-18 20:56:07 -0400

    fixed syntax errors

--- src/com/franklamar3d/FLomodoroClock/model/model/AttemptKind.java
+++ src/com/franklamar3d/FLomodoroClock/model/model/AttemptKind.java
@@ -3,14 +3,15 @@ package com.franklamar3d.FLomodoroClock.model.model;
 /**
  * Created by fl76at on 10/14/16.
  */
-public enum AttemptKind {
+
 public enum AttemptKind {
     FOCUS(25 * 60),
     BREAK(5 * 60);
 
     private int mTotalSeconds;
 
-    AttemptKind(int totalSeconds, String message) {
+    AttemptKind(int totalSeconds) {
+
         mTotalSeconds = totalSeconds;
     }
 
